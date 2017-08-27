5094229d8fc51ab80776f3dfc06516d35b6b1c51
subi300499 <30564474+subi300499@users.noreply.github.com>
2017-08-27 20:03:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 app.get('/article-one',function (req,res) {
-    res.send("Article one is requested");
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));  
 });
 app.get('/article-two',function (req,res) {
-    res.send("Article two is requested");
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function (req,res) {
-    res.send("Article three is requested");
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/', function (req, res) {
